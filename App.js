e3064d29516e66d7aead0102476bd3544d94eb3f
George <48507659+qeorqer@users.noreply.github.com>
2021-02-11 11:20:39 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import Header from './Components/header/Header';
+import Randomizer from "./Components/randomizer/Randomizer";
+import About from "./Components/about/About";
+import {Route, Switch} from "react-router-dom";
+
+function App() {
+    //states
+    let [options, setOptions] = useState([]);
+    let [selected, setSelected] = useState("");
+    let [showAnswer, setShowAnswer] = useState(false);
+    let [timer, setTimer] = useState(0);
+    let [banPoints, setBanPoints] = useState(0);
+
+    return (
+        <div className="App">
+            <Header/>
+            <main className="inner">
+                <Switch>
+                    <Route path='/' exact>
+                        <Randomizer banPoints = {banPoints} setBanPoints = {setBanPoints} options = {options} setOptions = {setOptions} selected={selected} setSelected={setSelected} showAnswer={showAnswer} setShowAnswer = {setShowAnswer} timer={timer} setTimer={setTimer}/>
+                    </Route>
+                    <Route path='/about'>
+                        <About/>
+                    </Route>
+                </Switch>
+                <div className={"banScore" + (banPoints > 0 ? " showScore" : "")}>
+                    {
+                        banPoints === 2 ? <p>Один шаг может изменить жизнь</p> :<p>Шагов до бана: <span>{3 - banPoints}</span></p>
+                    }
+
+                </div>
+            </main>
+        </div>
+    );
+}
+
+export default App;
